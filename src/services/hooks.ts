2205965210b54028db7dc2f2995bf2fcb66b350d
Masha Moreva <mashamiskova@yandex.ru>
2022-12-20 13:23:03 +0300

    добавлен хук useForm

--- src/services/hooks.ts
+++ src/services/hooks.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
@@ -11,3 +12,11 @@ import { RootState } from "./types/types";
 export const useDispatch = dispatchHook<ThunkDispatch<RootState, never, TUnionAction>>;
 export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
 
+export const useForm = (inputValues: {[key: string]: string}) => {
+  const [values, setValues] = React.useState(inputValues);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {value, name} = event.target;
+    setValues({...values, [name]: value});
+  };
+  return {values, handleChange, setValues};
+}
